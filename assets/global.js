6c7ad527499a840c715e4e6eb27b97e9b59eb77b
Bebe Jane <bjornthief@gmail.com>
2022-06-20 14:40:59 +0200

    menu

--- assets/global.js
+++ assets/global.js
@@ -1,1 +1,16 @@
 // global level js can go here
+
+
+window.addEventListener('load', (event) => {
+
+  var menu = document.getElementById('menu')
+  var menuToggle = document.getElementById('menu-toggle')
+  var nav = document.getElementById('nav')
+
+  menuToggle.addEventListener('click', function(e){
+    menuToggle.classList.toggle('menu--toggle-active')
+    nav.classList.toggle('nav-active')
+    menu.classList.toggle('menu-active')
+  })
+  
+});
